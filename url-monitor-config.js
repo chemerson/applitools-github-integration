9de98493b3ab580b78e4fa77db9bbe25702057fb
Chris Emerson <cme210@yahoo.com>
2022-03-02 11:38:54 -0500

    Update url-monitor-config.js

--- url-monitor-config.js
+++ url-monitor-config.js
@@ -9,7 +9,7 @@ const breakPoints = [1080]
 const orgName = 'Wikipedia'
 
 module.exports = {
-    change: 3,
+    change: 2,
     batchName: orgName + ' URLs',
     apiKey: process.env.APPLITOOLS_API_KEY,
     appName: orgName + ' UFG app',
